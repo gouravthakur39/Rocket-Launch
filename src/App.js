955c25a56fe14ff355a93bd816537aa48ad1240d
gouravthakur39 <gourav.thakur39@gmail.com>
2021-06-06 00:31:06 +0530

    404 page added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import { Fragment } from "react";
-import { Route } from "react-router-dom";
-import {AuthProvider} from './contexts/AuthContext'
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import { AuthProvider } from "./contexts/AuthContext";
 import SignUp from "../src/components/SignupPage/SignUp";
 import Login from "../src/components/LoginPage/Login";
 import Home from "./components/Home/Home";
 import ForgotPassword from "./components/ForgotPassword/ForgotPassword";
+import ErrorPage from "./components/ErrorPage/ErrorPage";
 
 function App() {
   return (
-    <AuthProvider>
-      <Fragment>
-        <Route exact path="/" component={Home} />
+    <Fragment>
+      <Router>
+        <AuthProvider>
+          <Switch>
+          <Route exact path="/" component={Home} />
 
-        <Route exact path="/signup" component={SignUp} />
+          <Route exact path="/signup" component={SignUp} />
 
-        <Route exact path="/login" component={Login} />
+          <Route exact path="/login" component={Login} />
 
-        <Route exact path="/reset" component={ForgotPassword} />
-      </Fragment>
-    </AuthProvider>
+          <Route exact path="/reset" component={ForgotPassword} />
+
+          <Route  path="/" component={ErrorPage} />
+
+          </Switch>
+        </AuthProvider>
+      </Router>
+    </Fragment>
   );
 }
 export default App;
